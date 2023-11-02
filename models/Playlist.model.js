bb7f5307ee91db6ebf1896d8140891c99423f88d
alextor26 <alextor2625@gmail.com>
2023-11-02 14:07:42 -0400

    Fixed issue with playlist model

--- models/Playlist.model.js
+++ models/Playlist.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const playlistSchema = new Schema(
   {
-    name: "My Songs",
+    name: {type: String, default:"My Songs"},
     songs: [{ type: Schema.Types.ObjectId, ref: "Songs" }],
   },
   {
