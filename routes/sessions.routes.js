eab80904b27b48cc9c01c9a5779a37424a540186
yankyalvarezm <alvarezyanky7#gmail.com>
2023-11-02 13:30:06 -0400

    added input validation to session route

--- routes/sessions.routes.js
+++ routes/sessions.routes.js
@@ -12,18 +12,16 @@ router.get("/", (req, res, next) => {
         : res.status(200).json({ success: true, sessionsArr: foundSessions });
     })
     .catch((error) => {
-      res
-        .status(400)
-        .json({
-          success: false,
-          error,
-          message: "Unable to Find any Session.",
-        });
+      res.status(400).json({
+        success: false,
+        error,
+        message: "Unable to Find any Session.",
+      });
     });
 });
 /* GET selected Session */
 router.get("/:sessionId", (req, res, next) => {
-  const {sessionId} = req.params
+  const { sessionId } = req.params;
   Session.findById(sessionId)
     .then((foundSession) => {
       !foundSession
@@ -31,38 +29,49 @@ router.get("/:sessionId", (req, res, next) => {
         : res.status(200).json({ success: true, session: foundSession });
     })
     .catch((error) => {
-      res
-        .status(400)
-        .json({
-          success: false,
-          error,
-          message: "Error trying to find Session.",
-        });
+      res.status(400).json({
+        success: false,
+        error,
+        message: "Error trying to find Session.",
+      });
     });
 });
 /* POST given a name, create a new Session */
 router.post("/create", isAuthenticated, (req, res, next) => {
   const { name } = req.body;
-  Session.create({
-    name
-  })
+
+  // Comprueba si el nombre está vacío o es solo espacios en blanco
+  if (!name || !name.trim()) {
+    return res.status(400).json({
+      success: false,
+      message: "Session name cannot be empty."
+    });
+  }
+
+  Session.create({ name })
     .then((createdSession) => {
-      res.status(201).json({ success: true, session: createdSession });
+      res.status(201).json({ success: true, session: createdSession, message: 'Session created' });
     })
     .catch((error) => {
       res
         .status(400)
         .json({ success: false, error, message: "Unable to create Session." });
+      console.log("Error:", error);
     });
 });
+
 
 /* PUT given a new name, update an existing Session name */
 router.put("/update/:sessionId", isAuthenticated, (req, res, next) => {
   const { name } = req.body;
   const { sessionId } = req.params;
-  Session.findByIdAndUpdate(sessionId, {
-    name,
-  }, {new: true})
+  Session.findByIdAndUpdate(
+    sessionId,
+    {
+      name,
+    },
+    { new: true }
+  )
     .then((updatedSession) => {
       res.status(201).json({ success: true, session: updatedSession });
     })
@@ -74,32 +83,48 @@ router.put("/update/:sessionId", isAuthenticated, (req, res, next) => {
 });
 
 /* POST given a userId, add a user to the Session */
-router.post('/add/:userId', isAuthenticated, (req,res,next) =>{
+router.post("/add/:userId", isAuthenticated, (req, res, next) => {
   const { sessionId } = req.params;
-  Session.findByIdAndUpdate(sessionId, {$addToSet: {users:userId}}, {new: true})
+  Session.findByIdAndUpdate(
+    sessionId,
+    { $addToSet: { users: userId } },
+    { new: true }
+  )
     .then((updatedSession) => {
       res.status(201).json({ success: true, session: updatedSession });
     })
     .catch((error) => {
       res
         .status(400)
-        .json({ success: false, error, message: "Unable to add user to Session." });
+        .json({
+          success: false,
+          error,
+          message: "Unable to add user to Session.",
+        });
     });
-})
+});
 
 /* POST given a userId, remove a user from the Session */
-router.post('/remove/:userId', isAuthenticated, (req,res,next) =>{
+router.post("/remove/:userId", isAuthenticated, (req, res, next) => {
   const { sessionId } = req.params;
-  Session.findByIdAndUpdate(sessionId, {$pull: {users:userId}}, {new: true})
+  Session.findByIdAndUpdate(
+    sessionId,
+    { $pull: { users: userId } },
+    { new: true }
+  )
     .then((updatedSession) => {
       res.status(201).json({ success: true, session: updatedSession });
     })
     .catch((error) => {
       res
         .status(400)
-        .json({ success: false, error, message: "Unable to remove user from Session." });
+        .json({
+          success: false,
+          error,
+          message: "Unable to remove user from Session.",
+        });
     });
-})
+});
 
 /* Delete given a sessionId, delete the Session */
 router.delete("/delete/:sessionId", (req, res, next) => {
