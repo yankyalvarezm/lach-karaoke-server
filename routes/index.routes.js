693a64c96de777d54ddd708a6272a98fc601c1f3
alextor26 <alextor2625@gmail.com>
2023-11-03 12:53:18 -0400

    Finished working on songs routes, playlists and songs routes need testing.

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -11,9 +11,11 @@ router.get("/", (req, res, next) => {
         : res.status(200).json({ success: true, message: "No Songs found." });
     })
     .catch((error) => {
-      res
-        .status(400)
-        .json({ success: false, error, message: "Error: Unable to GET Songs." });
+      res.status(400).json({
+        success: false,
+        error,
+        message: "Error: Unable to GET Songs.",
+      });
     });
 });
 /* GET a song by songId. */
@@ -42,28 +44,61 @@ router.post("/create", (req, res, next) => {
       genre,
     },
     { new: true }
-  ).then((createdSong) => {
-    createdSong
-      ? res.status(201).json({ success: true, song: createdSong })
-      : res
-          .status(200)
-          .json({ success: true, message: "Failed to create song." });
-  })
-  .catch((error) => {
-    res
-      .status(400)
-      .json({ success: false, error, message: "Error: Unable create Song in POST." });
-  });
+  )
+    .then((createdSong) => {
+      createdSong
+        ? res.status(201).json({ success: true, song: createdSong })
+        : res
+            .status(200)
+            .json({ success: true, message: "Failed to create song." });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        success: false,
+        error,
+        message: "Error: Unable create Song in POST.",
+      });
+    });
+});
+
+/* PUT given a title, artist, genre and given an songId, update selected song. */
+router.put("/update/:songId", (req, res, next) => {
+  const { songId } = req.params;
+  const { title, artist, genre } = req.body;
+  Song.findByIdAndUpdate(songId, { title, artist, genre }, { new: true })
+    .then((updatedSong) => {
+      updatedSong
+        ? res.status(200).json({ success: true, song: updatedSong })
+        : res
+            .status(200)
+            .json({ success: true, message: "Failed to update song." });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        success: false,
+        error,
+        message: "Error: Unable update Song in PUT.",
+      });
+    });
 });
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.status(200).json();
-  res.status(400).json({ success: true, error, message: "" });
-});
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  res.status(200).json();
-  res.status(400).json({ success: true, error, message: "" });
+router.delete("/delete/:songId", (req, res, next) => {
+  const { songId } = req.params;
+  Song.findByIdAndDelete(songId)
+    .then((deletedSong) => {
+      deletedSong
+        ? res.status(200).json({ success: true, song: deletedSong })
+        : res.status(400).json({ success: true, message: "Song not found." });
+    })
+    .catch((error) => {
+      res
+        .status(400)
+        .json({
+          success: false,
+          error,
+          message: "Error: Failed to DELETE song "
+        });
+    });
 });
 
 module.exports = router;
