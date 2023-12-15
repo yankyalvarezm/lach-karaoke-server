4f549b18cf18ddde1d6780567603e21a05307f10
John Doe <johndoe@example.com>
2023-12-14 20:10:15 -0400

    now we are using a global borwser instead of opening a new one for each video

--- routes/youtube.routes.js
+++ routes/youtube.routes.js
@@ -1,90 +1,87 @@
 var express = require("express");
 var router = express.Router();
-const { google } = require('googleapis')
+const { google } = require('googleapis');
 const puppeteer = require("puppeteer");
-const path = require('path')
 
+// Configuración de la API de YouTube
 const youtube = google.youtube({
     version: 'v3',
     auth: process.env.YOUTUBE_API_KEY 
-  });
-  
-  const checkVideoExistence = async (videoId) => {
-    let browser = null;
+});
 
-    // const userDataDir = path.join(__dirname, 'puppeteer_user_data');
-  
+// Iniciar la instancia global del navegador
+let globalBrowser;
+(async () => {
+    globalBrowser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+})();
+
+// Función para verificar la existencia de un video
+const checkVideoExistence = async (videoId) => {
+    let page;
+
     try {
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            // userDataDir: userDataDir 
+        page = await globalBrowser.newPage();
+        page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+        await page.goto(`https://www.youtube.com/embed/${videoId}`);
+
+        const isUnavailable = await page.evaluate(() => {
+            const elem = document.body;
+            if (elem) {
+                console.log("INNER TEXT:", elem.innerText);
+            }
+            return elem && (elem.innerText.includes("Video unavailable") || elem.innerText.includes("Video no disponible"));
         });
 
-        // browser = await puppeteer.launch({ headless: true });
-
-      const page = await browser.newPage();
-      page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
-  
-      // Usar URL de incrustación para la verificación
-      await page.goto(`https://www.youtube.com/embed/${videoId}`);
-  
-      const isUnavailable = await page.evaluate(() => {
-        const elem = document.body;
-        if(elem) {
-            console.log("INNER TEXT:", elem.innerText);
-        }
-        return elem && (elem.innerText.includes("Video unavailable") || elem.innerText.includes("Video no disponible")) ;
-      });
-  
-      return !isUnavailable;
+        return !isUnavailable;
     } catch (error) {
-      console.error("Web Scraping Error:", error);
-      return false;
+        console.error("Web Scraping Error:", error);
+        return false;
     } finally {
-      if (browser) {
-        await browser.close();
-      }
-    }
-  };
-  
-  
-  router.get('/search/videos', async (req, res) => {
-    const query = req.query.q; 
-  
-    try {
-      const response = await youtube.search.list({
-        part: 'snippet',
-        q: query,
-        videoEmbeddable: 'any',
-        maxResults: 15
-      });
-  
-      const videos = response.data.items;
-      const validVideos = [];
-  
-      for (const video of videos) {
-        const videoId = video.id.videoId;
-        const isAvailable = await checkVideoExistence(videoId);
-        if (isAvailable) {
-          validVideos.push(video);
+        if (page) {
+            await page.close();
         }
-      }
-  
-      res.json({ items: validVideos });
-    } catch (err) {
-      console.error('Error en la búsqueda de videos de YouTube:', err);
-      res.status(500).json({
-        success: false,
-        message: 'Error al realizar la búsqueda en YouTube',
-        error: err.message 
-      });
     }
-  });
+};
 
+// Rutas
+router.get('/search/videos', async (req, res) => {
+    const query = req.query.q;
 
-  router.get('/video/details', (req, res) => {
-    const videoId = req.query.id; 
+    try {
+        const response = await youtube.search.list({
+            part: 'snippet',
+            q: query,
+            videoEmbeddable: 'any',
+            maxResults: 15
+        });
+
+        const videos = response.data.items;
+        const validVideos = [];
+
+        for (const video of videos) {
+            const videoId = video.id.videoId;
+            const isAvailable = await checkVideoExistence(videoId);
+            if (isAvailable) {
+                validVideos.push(video);
+            }
+        }
+
+        res.json({ items: validVideos });
+    } catch (err) {
+        console.error('Error en la búsqueda de videos de YouTube:', err);
+        res.status(500).json({
+            success: false,
+            message: 'Error al realizar la búsqueda en YouTube',
+            error: err.message 
+        });
+    }
+});
+
+router.get('/video/details', (req, res) => {
+    const videoId = req.query.id;
 
     if (!videoId) {
         return res.status(400).send({ message: 'Se requiere el ID del video' });
@@ -105,6 +102,13 @@ const youtube = google.youtube({
             res.status(404).send({ message: 'Video no encontrado' });
         }
     });
+});
+
+// Cerrar el navegador al terminar el proceso
+process.on('exit', () => {
+    if (globalBrowser) {
+        globalBrowser.close();
+    }
 });
 
 module.exports = router;
